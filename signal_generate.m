62e8ceea97cc8c6cc90c60d95cbee384c3cb58fe
MeSagor <dear.shorifulislam@gmail.com>
2022-11-15 22:48:39 +0600

    unit-impulse,unit-step,unit-ramp

--- signal_generate.m
+++ signal_generate.m
@@ -1,11 +1,25 @@
 clc; close all; clear all;
 
-t = 0: 0.01: 1; % discrete time
-f = 4;  % frequency(hz)
-a = 5;  % amplitude
+% Unit impulse signal
+t1 = -20:20;
+start_point_impulse = 0;
+impulse = (t1 - start_point_impulse == 0);
+subplot(311);
+stem(t1, impulse, 'LineWidth',1);
+title('Unit Impulse Signal');
 
-signal = a * sin(2 * pi * f * t);
+% Unit step signal;
+t2 = -20:20;
+start_point_step = 0;
+unit_step = (t2 - start_point_step >= 0);
+subplot(312);
+stem(t2, unit_step, 'LineWidth',1);
+title('Unit Step Signal');
 
-plot(t, signal);
-
-
+% Unit ramp signal;
+t3 = -20:20;
+start_point_ramp = 0;
+unit_ramp = t3 .* (t3 - start_point_ramp >= 0);
+subplot(313);
+stem(t3, unit_ramp, 'LineWidth',1);
+title('Unit Ramp Signal');
